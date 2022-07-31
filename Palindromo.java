307d399626d0867933eb509cffb3351293760b2d
Danielly Nunes <danym.nunes@gmail.com>
2022-07-31 02:22:33 -0300

    quarta adição

--- Palindromo.java
+++ Palindromo.java
@@ -1,3 +1,8 @@
+/*Dado uma string, rearranje seus caracteres de forma que a string se torne um palíndromo (pode ser lido da esquerda para a direita 
+ ou da direita para a esquerda). A entrada consiste de umas string composta de apenas letras em caixa alta (A-Z).
+Considere que sempre será possível transformar a string em um palíndromo.
+LINK: https://sandbox.letscode.com.br/exercicio/0de90875-62c3-4cb3-8dc7-779606c81671*/
+
 package exemplos_diversos;
 
 import java.util.Scanner;
@@ -7,7 +12,7 @@ public class Palindromo {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 
-		System.out.println("Digite uma palavra ou frase: ");
+		System.out.println("Digite uma palavra: ");
 		String palavra = leia.nextLine();
 		String palindromo = ""; 
 
