9ca7ff7b957f2402a7540dd9fdae3c98989d3fee
Danielly Nunes <danym.nunes@gmail.com>
2022-08-29 23:34:07 -0300

    quinta adição

---
+++ Tabuada.java
@@ -0,0 +1,21 @@
+package treinamento;
+
+import java.util.Scanner;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		int numero = 0, resultado, x;
+
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite um número para ver sua tabuada: ");
+		numero = ler.nextInt();
+
+		for (x = 1; x <= 10; x++) {
+			resultado = numero*x;
+			System.out.println(numero+" x "+x+" = "+resultado);
+
+		}
+
+	}
+}
